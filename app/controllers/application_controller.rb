618f2ab05bc5171259ea475991cd0fd3d78b5378
JustinMitchell <mitchelljm04@gmail.com>
2022-12-13 17:23:30 -0500

    Assignment Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_error
+  before_action :authorize
+
+
+private 
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: {errors: ["Not Authorized"]}, status: 401 unless @current_user  
+  end
+
+
+  def handle_error exception
+    render json: {errors: exception.record.errors.full_messages}, status: 422
+  end
 
 end
